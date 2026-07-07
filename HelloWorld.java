c8e6dc118b0e9454c8f0994af1e15ea64cc235a1
mapena111 <olomomoses75@gmail.com>
2026-07-07 12:38:33 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package com.adse2509.sess01_java_intro;
+
+/**
+ *
+ * @author mooh
+ */
+public class HelloWorld
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Nyanjui from Java!");
+    }
+    
+}
---
+++ OperatorPrecedence.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.adse2509.sess02_variables_datatypes_operators;
+
+/**
+ *
+ * @author mooh
+ */
+public class OperatorPrecedence
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
